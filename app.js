04d328f23194f35abb02eaa0e3a1e8bc5e43e3a4
rodricaba22 <rodricabanes10@gmail.com>
2023-05-15 21:47:31 -0300

    Port servidor

--- app.js
+++ app.js
@@ -4,14 +4,24 @@ const path = require('path');
 
 const app = express();
 
+const PORT=process.env.PORT || 3010;
+ 
+
+
 app.use(express.static('public'))
 app.use(express.static('views'))
 
-app.listen(3010, () => {
-    console.log('Servidor corriendo en el puerto 3010')
+app.listen(PORT, () => {
+    console.log('Servidor corriendo en el puert'+ PORT)
 });
 
 app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname,'/views/home.html'));
+});
+app.get('/register', (req,res) => {
+    res.sendFile(path.join(__dirname,'/views/register.html'));
+});
+app.get('/login', (req,res) => {
+    res.sendFile(path.join(__dirname,'/views/login.html'));
 });
 
